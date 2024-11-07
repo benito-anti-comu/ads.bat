67a50a36aae3f0495b34fea3dd2b473faa37f913
benito-anti-comu <dfg792242@gmail.com>
2024-11-07 18:18:10 +0100

    Add files via upload
    le pubblicità sono belle.

---
+++ Ads.bat
@@ -0,0 +1,66 @@
+@echo off
+color b
+title ANTI VIRUS AFFIDABILI
+echo.
+echo.
+echo.
+echo            Prova pure tu ANTIVIRUSSICURISSIMO.exe per non ricevere virus
+echo                       INSTALLA ORA!!!!!!!!!!!!!!!!!!!!!!!!
+echo.
+echo                     il ANTIVIRUSSICURISSIMO.exe per funzionare 
+echo         devi dare il tuo account gmail e la tua passworld, ti puoi fidare;)
+echo.
+echo.
+echo  Premere per installare ANTIVIRUSSICURISSIMO.exe...
+pause >nul
+cls
+color c
+echo.
+echo.
+echo.
+echo                                 accetta i cookies!!!
+echo          Per utilizzare ANTIVIRUSSICURISSIMO.exe devi accettare i cookies e 
+echo          devi firmare con il sangue, spiega che il tuo sangue e tuo, poi firma
+echo      per dare le informazioni al GOVERNO COMUNISTA CINESE, grazie e 
+echo                                  buona giornata;)
+echo.
+set /p sangue="Firma con il sangue -> "
+echo analizzazione del sangue...
+timeout /t 10 /nobreak >nul
+set /p firma="mettila firma per IL GOVERNO COMUNISTA CINESE -> "
+echo verificazione della firma...
+timeout /t 10 /nobreak >nul
+echo fatto, ora puoi installare ANTIVIRUSSICURISSIMO.exe
+pause
+cls
+color b
+cd desktop
+md ANTIVIRUSSICURISSIMO.exe
+cd ANTIVIRUSSICURISSIMO.exe
+echo @echo off > ANTIVIRUSSICURISSIMO.bat
+echo title ANTIVIRUSSICURISSIMO.exe >> ANTIVIRUSSICURISSIMO.bat
+echo color 2 >> ANTIVIRUSSICURISSIMO.bat
+echo echo stiamo pulendo il pc... >> ANTIVIRUSSICURISSIMO.bat
+echo timeout /t 10 /nobreak >nul >> ANTIVIRUSSICURISSIMO.bat
+echo color c >> ANTIVIRUSSICURISSIMO.bat
+echo echo VIRSU TROVATO!!! >> ANTIVIRUSSICURISSIMO.bat
+echo timeout /t 1 /nobreak >nul >> ANTIVIRUSSICURISSIMO.bat
+echo echo NOME VIRUS, tu.exe >> ANTIVIRUSSICURISSIMO.bat
+echo shutdown -r -c "HAHAHAHAHAHAHAHAH" -t 2 >> ANTIVIRUSSICURISSIMO.bat
+echo pause >> ANTIVIRUSSICURISSIMO.bat
+echo @echo off > ANTIVIRUS.bat
+echo title HOOOOOOO >> ANTIVIRUS.bat
+echo echo pensi che ho voglia? >> ANTIVIRUS.bat
+echo pause >nul >> ANTIVIRUS.bat
+echo @echo off > VIRUSNO.bat
+echo title HOOOOOOO >> VIRUSNO.bat
+echo echo pensi che ho voglia? >> VIRUSNO.bat
+echo pause >> VIRUSNO.bat
+echo @echo off > NOVIRUSNO.bat
+echo title HOOOOOOO >> NOVIRUSNO.bat
+echo echo pensi che ho voglia? >> NOVIRUSNO.bat
+echo pause >> NOVIRUSNO.bat
+echo @echo off > HOOO.bat
+echo title HOOOOOOO >> HOOO.bat
+echo echo pensi che ho voglia? >> HOOO.bat
+echo pause >> HOOO.bat
